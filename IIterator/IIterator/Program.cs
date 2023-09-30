a7b3f2b900087ed548ec59840bd78eb83bd167ca
3D23 <dmitrichenkov.dima@mail.ru>
2023-09-30 12:33:11 +0300

    Add iterator and yield

---
+++ IIterator/IIterator/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+
+namespace iterator;
+public class Car
+{
+    public string Name { get; private set; }
+
+    public Car(string name)
+    {
+        Name = name;
+    }
+}
+
+public class Cars : IEnumerable
+{
+    private readonly Car[] cars;
+
+    public Cars(Car[] cars)
+    {
+        this.cars = new Car[cars.Length];
+
+        for (int i = 0; i < cars.Length; i++) 
+        {
+            this.cars[i] = cars[i];
+        }
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        return new CarsEnum(cars);
+    }
+}
+
+public class CarsEnum : IEnumerator
+{
+    private readonly Car[] cars;
+
+    public object Current => cars[position];
+
+    private int position = -1;
+
+    public CarsEnum(Car[] cars) 
+    {
+        this.cars = cars;
+    }
+
+    public bool MoveNext()
+    {
+        position++;
+        return position < cars.Length;
+    }
+
+    public void Reset()
+    {
+        position = -1;
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Car[] cars = new Car[3]
+        {
+            new Car("mazda"),
+            new Car("honda"),
+            new Car("bugatti")
+        };
+
+        Cars carsEnum = new(cars);
+
+        foreach (Car car in carsEnum) 
+        {
+            Console.WriteLine(car.Name);
+        }
+    }
+}
---
+++ Yield/Yield/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+
+class Program
+{
+    static IEnumerator GetInts()
+    {
+        Console.WriteLine("first");
+        yield return 1;
+
+        Console.WriteLine("second");
+        yield return 2;
+    }
+
+    static IEnumerator GetMultiplicationsInts(int maxValue)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            for (int  j = 0; j < 10; j++)
+            {
+                int result = i * j;
+
+                if (result > maxValue)
+                    yield break;
+
+                yield return result;
+            }
+        }
+    }
+
+    static IEnumerable GetFibonacci(int maxValue)
+    {
+        int previous = 0;
+        int current = 1;
+
+        while (current < maxValue)
+        {
+            yield return current;
+
+            int newCurrent = previous + current;
+            previous = current;
+            current = newCurrent;
+        }
+    }
+    static void PrintFibonacci()
+    {
+        Console.WriteLine("Fibonacci numbers: ");
+
+        foreach (int number in GetFibonacci(100))
+        {
+            Console.WriteLine(number);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        /*IEnumerator enumerator = GetInts();
+        Console.WriteLine("---");
+        enumerator.MoveNext();
+        Console.WriteLine(enumerator.Current);*/
+      
+        /*IEnumerator enumerator = GetMultiplicationsInts(75);
+        while (enumerator.MoveNext())
+        {
+            Console.WriteLine(enumerator.Current);
+        }*/
+
+        /*PrintFibonacci();*/
+
+    }
+}
